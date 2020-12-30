3c4ec0ff3da51a7ab5e1712dc7ce9a4672cdb7c7
Marvin Scharle <marvin.scharle@conclurer.com>
2020-12-30 23:53:16 +0100

    [add] debug statements

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -5,6 +5,10 @@ require 'parseconfig'
 
 config = ParseConfig.new("#{ENV['BUILD_DIR']}/.gitmodules")
 
+ARGV.each do |a|
+  puts "Argument: #{a}"
+end
+
 config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
